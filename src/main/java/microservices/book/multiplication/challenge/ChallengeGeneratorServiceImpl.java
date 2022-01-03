c414d902c9b3740adf42fd3e2eaa57bb655ae3d1
Skiminok <skiminokd@gmail.com>
2022-01-03 22:26:28 +0300

    Добавлен сервис ChallengeGeneratorService и тест для него

---
+++ src/main/java/microservices/book/multiplication/challenge/ChallengeGeneratorServiceImpl.java
@@ -0,0 +1,24 @@
+package microservices.book.multiplication.challenge;
+
+import org.springframework.stereotype.Service;
+import java.util.Random;
+import java.util.random.RandomGenerator;
+
+@Service
+public class ChallengeGeneratorServiceImpl implements ChallengeGeneratorService {
+
+    private final RandomGenerator randomGenerator;
+
+    ChallengeGeneratorServiceImpl() {
+        this.randomGenerator = new Random();
+    }
+
+    protected ChallengeGeneratorServiceImpl(final RandomGenerator randomGenerator) {
+        this.randomGenerator = randomGenerator;
+    }
+
+    @Override
+    public Challenge randomChallenge() {
+        return null;
+    }
+}
