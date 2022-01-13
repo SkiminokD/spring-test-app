d87da0fdd25bcd73e4d9023bf8e4c0f0fc5f6304
Skiminok <skiminokd@gmail.com>
2022-01-13 18:02:08 +0300

    Реализовано сохранение пользователей и их попыток в базу данных h2

--- src/main/java/microservices/book/multiplication/user/User.java
+++ src/main/java/microservices/book/multiplication/user/User.java
@@ -2,14 +2,23 @@ package microservices.book.multiplication.user;
 
 import lombok.*;
 
+import javax.persistence.*;
+
 /**
  * Stores information to identify the user
  */
-@Getter
-@ToString
-@EqualsAndHashCode
+@Entity
+@Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue
     private Long id;
     private String alias;
+
+    public User(final String userAlias) {
+        this(null, userAlias);
+    }
 }
